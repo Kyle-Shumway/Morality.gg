9ab4ddde863b6f9f4e1504208cbb7e7fc68a6833
Start Coding Student 2 <session2@START-Coding-MBP-14.local>
2016-05-10 16:00:26 -0700

    About.php & faq.php

---
+++ faq.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Morality.gg</title>
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+<div id="faq">
+<body background="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgiesI7Zmpi6QZmf1h2OkyveUr5dK2t7PfDEjrUku4fsPOCbYeuQ">
+
+<table border="1px" width="100%">
+    <tr>
+        <td>F.A.Q</td>
+    </tr>
+</table>
+
+</body>
+</div>
+</html>
+
